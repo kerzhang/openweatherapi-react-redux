27eb703f5e641e588469de5e79deadafcd2a633d
Ker Zhang <kerzhang@live.com>
2017-09-07 22:46:11 +0800

    initial

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+import React, { Component } from 'react';
+
+import BookList from '../containers/book-list';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BookList />
+      </div>
+    );
+  }
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,16 @@
+export default function() {
+  return [
+    {
+      title: 'Javascrit: The Good Parts'
+    },
+    {
+      title: 'Hary Potter'
+    },
+    {
+      title: 'Python: The Dark Tower'
+    },
+    {
+      title: 'What is Goods'
+    }
+  ];
+}
